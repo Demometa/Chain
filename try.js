4aff6001022c81b973dd21ce199e226484b009ce
Meta <126449463+Demometa@users.noreply.github.com>
2023-07-11 16:02:56 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,28 +1,30 @@
 const express = require("express");
-const Web3 = require("web3");
+const { ethers } = require("ethers");
 
 const app = express();
 app.use(express.json());
 
 app.post("/chain", async (req, res) => {
-    const chainId = 123;
-    try {
-      const {  jsonrpc,method,params,id } = req.body;
-    
-  
-      if (jsonrpc !== "2.0") {
-        return res.status(400).json({ error: "jsonrpc version mismatched" });
-      }
-      if (method == "eth_chainId") {
-        res.json({ jsonrpc: "2.0",  result: `0x${chainId.toString(16)}`,id:1 });
-      } else {
-        res.status(400).json({ error: "Invalid Chain ID for the specified RPC URL." });
-      }
-    } catch (error) {
-        console.error("Error handling the request:", error);
-        res.status(500).json({ error: error.message });
-      }
-    
+  const chainId = 123;
+  const name = "Metavy";
+
+  try {
+    const { jsonrpc, method, params, id } = req.body;
+
+    if (jsonrpc !== "2.0") {
+      return res.status(400).json({ error: "jsonrpc version mismatched" });
+    }
+    if (method == "eth_chainId") {
+      res.json({ jsonrpc: "2.0", result: `0x${chainId.toString(16)}`, id: 1 });
+    } else {
+      res
+        .status(400)
+        .json({ error: "Invalid Chain ID for the specified RPC URL." });
+    }
+  } catch (error) {
+    console.error("Error handling the request:", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const port = 7000;
